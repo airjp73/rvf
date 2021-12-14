217c40dbe6ccd0c853613d46f3fcfe8d1c2bcdf1
Aaron Pettengill <pettengill.aaron@gmail.com>
2021-12-14 18:18:11 -0500

    Move linting stuff to root folder

--- packages/remix-validated-form/jest.config.js
+++ packages/remix-validated-form/jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
   preset: "ts-jest",
   testEnvironment: "node",
 };
