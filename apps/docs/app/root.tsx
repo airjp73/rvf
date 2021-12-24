a4593d5b06fa51cb537737e662bc48efce5225ed
Aaron Pettengill <pettengill.aaron@gmail.com>
2021-12-24 13:55:10 -0500

    Add tailwind to docs site

--- apps/docs/app/root.tsx
+++ apps/docs/app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
@@ -7,10 +8,15 @@ import {
   ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import stylesUrl from "./styles.css";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
 
 export default function App() {
   return (
--- apps/docs/app/routes/index.tsx
+++ apps/docs/app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-blue-500">Welcome to Remix</h1>
       <ul>
         <li>
           <a
