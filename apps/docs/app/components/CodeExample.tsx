9b0927a42701839e9f9efe53d39826acafbac032
Aaron Pettengill <pettengill.aaron@gmail.com>
2021-12-26 16:39:00 -0500

    Add CodeExample component

--- apps/docs/app/components/CodeExample.tsx
+++ apps/docs/app/components/CodeExample.tsx
@@ -1,5 +1,26 @@
-import { FC } from "react";
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
+import { Children, FC } from "react";
 
-export const CodeExample: FC = ({ children }) => {
-  return <div className="not-prose">{children}</div>;
+type CodeExampleProps = {
+  title: string;
+};
+
+export const CodeExample: FC<CodeExampleProps> = ({ children, title }) => {
+  if (Children.count(children) !== 2) throw new Error("Need example and code");
+  const [example, code] = Children.toArray(children);
+
+  return (
+    <div className="not-prose code-example">
+      <Tabs variant="soft-rounded">
+        <TabList>
+          <Tab>Example</Tab>
+          <Tab>Code</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>{example}</TabPanel>
+          <TabPanel>{code}</TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
 };
