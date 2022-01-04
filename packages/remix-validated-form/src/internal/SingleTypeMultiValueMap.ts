51bcaf4758e316e23fa89b5238ee7b453a576151
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-01-03 22:40:25 -0500

    Add internal multivalue map class & hook

---
+++ packages/remix-validated-form/src/internal/SingleTypeMultiValueMap.ts
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+
+export class MultiValueMap<Key, Value> {
+  private dict: Map<Key, Value[]> = new Map();
+
+  add = (key: Key, value: Value) => {
+    this.dict.set(key, [...(this.dict.get(key) ?? []), value]);
+    if (this.dict.has(key)) {
+      this.dict.get(key)!.push(value);
+    } else {
+      this.dict.set(key, [value]);
+    }
+  };
+
+  remove = (key: Key, value: Value) => {
+    if (!this.dict.has(key)) return;
+    const array = this.dict.get(key)!;
+    const index = array.indexOf(value);
+    if (index !== -1) array.splice(index, 1);
+    if (array.length === 0) this.dict.delete(key);
+  };
+
+  getAll = (key: Key): Value[] => {
+    return this.dict.get(key) ?? [];
+  };
+
+  has = (key: Key): boolean => this.dict.has(key);
+}
+
+export const useMultiValueMap = <Key, Value>() => {
+  const ref = useRef<MultiValueMap<Key, Value> | null>(null);
+  return () => {
+    if (ref.current) return ref.current;
+    ref.current = new MultiValueMap();
+    return ref.current;
+  };
+};
