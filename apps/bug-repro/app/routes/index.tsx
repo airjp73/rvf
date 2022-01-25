31212a494f6b3750dcff33f6d2a65329ae308354
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-01-24 19:04:01 -0500

    fix: Change text

--- apps/bug-repro/app/routes/index.tsx
+++ apps/bug-repro/app/routes/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Bug reproduction template</h1>
-      <p>Edit this CodeSandbox and share it in the issue</p>
+      <p>Fork and edit this CodeSandbox and share it in the issue</p>
     </div>
   );
 }
