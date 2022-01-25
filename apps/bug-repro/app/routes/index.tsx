da7e1a5e14f1dbd8f47339a15436cd3615cf20e8
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-01-24 19:01:50 -0500

    feat: add bug reproduction app

---
+++ apps/bug-repro/app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Bug reproduction template</h1>
+      <p>Edit this CodeSandbox and share it in the issue</p>
+    </div>
+  );
+}
