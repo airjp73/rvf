7ee4d64d3a03621811cd299085d9ac618c7fcffd
Tom Rowe <theflyingcoder@hotmail.com>
2022-01-31 10:42:33 +1000

    Add Async validation support to withZod validator

--- packages/with-zod/src/index.ts
+++ packages/with-zod/src/index.ts
@@ -20,6 +20,30 @@ function pathToString(array: (string | number)[]): string {
   }, "");
 }
 
+const getValidateResult = <T>(result: z.SafeParseReturnType<unknown, T>) => {
+  if (result.success) return { data: result.data, error: undefined };
+
+  const fieldErrors: FieldErrors = {};
+  getIssuesForError(result.error).forEach((issue) => {
+    const path = pathToString(issue.path);
+    if (!fieldErrors[path]) fieldErrors[path] = issue.message;
+  });
+  return { error: fieldErrors, data: undefined };
+};
+
+const getValidateFieldResult = <T>(
+  result: z.SafeParseReturnType<unknown, T>,
+  field: string
+) => {
+  if (result.success) return { error: undefined };
+  return {
+    error: getIssuesForError(result.error).find((issue) => {
+      const allPathsAsString = issue.path.map((p) => `${p}`);
+      return isEqual(allPathsAsString, toPath(field));
+    })?.message,
+  };
+};
+
 /**
  * Create a validator using a `zod` schema.
  */
@@ -29,24 +53,19 @@ export function withZod<T, U>(
   return createValidator({
     validate: (value) => {
       const result = zodSchema.safeParse(value);
-      if (result.success) return { data: result.data, error: undefined };
-
-      const fieldErrors: FieldErrors = {};
-      getIssuesForError(result.error).forEach((issue) => {
-        const path = pathToString(issue.path);
-        if (!fieldErrors[path]) fieldErrors[path] = issue.message;
-      });
-      return { error: fieldErrors, data: undefined };
+      return getValidateResult(result);
+    },
+    validateAsync: async (value) => {
+      const result = await zodSchema.safeParseAsync(value);
+      return getValidateResult(result);
     },
     validateField: (data, field) => {
       const result = zodSchema.safeParse(data);
-      if (result.success) return { error: undefined };
-      return {
-        error: getIssuesForError(result.error).find((issue) => {
-          const allPathsAsString = issue.path.map((p) => `${p}`);
-          return isEqual(allPathsAsString, toPath(field));
-        })?.message,
-      };
+      return getValidateFieldResult(result, field);
+    },
+    validateFieldAsync: (data, field) => {
+      const result = zodSchema.safeParse(data);
+      return getValidateFieldResult(result, field);
     },
   });
 }
