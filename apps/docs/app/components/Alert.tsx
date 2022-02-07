5f9cd7c7feb0a6fa864fe2a29a21a26c53ee6d97
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-02-06 22:45:39 -0500

    docs: add warning variant for alert

--- apps/docs/app/components/Alert.tsx
+++ apps/docs/app/components/Alert.tsx
@@ -1,12 +1,17 @@
 import {
   CheckCircleIcon,
+  ExclamationIcon,
   InformationCircleIcon,
   XCircleIcon,
 } from "@heroicons/react/solid";
 import classNames from "classnames";
 import { FC, ReactNode } from "react";
 
-export type AlertVariants = "error" | "info" | "success";
+export type AlertVariants =
+  | "error"
+  | "info"
+  | "success"
+  | "warning";
 
 export type AlertProps = {
   title?: string;
@@ -20,6 +25,7 @@ const variantIcons = {
   error: XCircleIcon,
   info: InformationCircleIcon,
   success: CheckCircleIcon,
+  warning: ExclamationIcon,
 };
 
 export const Alert: FC<AlertProps> = ({
@@ -37,6 +43,8 @@ export const Alert: FC<AlertProps> = ({
         "rounded-md p-4 border border-red-400",
         variant === "error" && "border-red-400 bg-red-50",
         variant === "info" && "border-blue-400 bg-blue-50",
+        variant === "warning" &&
+          "border-yellow-400 bg-yellow-50",
         variant === "success" &&
           "border-green-400 bg-green-50",
         className
@@ -49,6 +57,7 @@ export const Alert: FC<AlertProps> = ({
               "h-5 w-5",
               variant === "error" && "text-red-500",
               variant === "info" && "text-blue-500",
+              variant === "warning" && "text-yellow-500",
               variant === "success" && "text-green-500"
             )}
           />
@@ -60,6 +69,7 @@ export const Alert: FC<AlertProps> = ({
                 "text-sm font-medium",
                 variant === "error" && "text-red-800",
                 variant === "info" && "text-blue-800",
+                variant === "warning" && "text-yellow-800",
                 variant === "success" && "text-green-800"
               )}
             >
@@ -73,6 +83,7 @@ export const Alert: FC<AlertProps> = ({
                 !!title && "mt-2",
                 variant === "error" && "text-red-700",
                 variant === "info" && "text-blue-700",
+                variant === "warning" && "text-yellow-700",
                 variant === "success" && "text-green-700"
               )}
             >
