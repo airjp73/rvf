bb5b2e98daaf7a65bee7af4bee22fcd631846508
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-02-24 19:14:24 -0500

    docs: add docs for new helpers
    Probably won't add these to the nav until official release

---
+++ apps/docs/app/components/DeprecatedWarning.tsx
@@ -0,0 +1,30 @@
+import { Link } from "remix";
+import { Alert } from "./Alert";
+
+export const DeprecatedWarning = () => (
+  <div className="not-prose">
+    <Alert
+      variant="warning"
+      title="Deprecated"
+      details={
+        <>
+          useFormContext is deprected in favor of{" "}
+          <Link
+            to="/reference/use-form-state"
+            className="text-blue-500"
+          >
+            useFormState
+          </Link>{" "}
+          and{" "}
+          <Link
+            to="/reference/use-form-helpers"
+            className="text-blue-500"
+          >
+            useFormHelpers
+          </Link>
+          .
+        </>
+      }
+    />
+  </div>
+);
