e1cefa9decc05eb7fc9056777a0984f6136187b8
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-05-07 17:15:43 -0400

    docs: add some doc blocks

--- packages/experimental-validation/src/helpers/number.ts
+++ packages/experimental-validation/src/helpers/number.ts
@@ -1,6 +1,12 @@
 import { check, transform, typecheck } from "../core";
 import { ErrorMessage, errorMessage } from "../errors";
 
+/**
+ * Checks if the value is a number.
+ *
+ * @param typeError - Optionally specify an error message to use in this case.
+ * @returns A validation pipeline that checks if the value is a number.
+ */
 export const number = (typeError?: ErrorMessage) =>
   typecheck(
     (val): val is number => typeof val === "number",
@@ -11,8 +17,19 @@ export const number = (typeError?: ErrorMessage) =>
       "Expected a number"
     )
   );
+
+/**
+ * The meta key used to store the error message for this validation.
+ */
 number.ERROR = Symbol("number_error");
 
+/**
+ * Checks if a number is less than or equal to the maximum.
+ *
+ * @param maximum - The maximum value.
+ * @param error - Optionally specify an error message to use in this case.
+ * @returns A validation pipeline that checks if a number is less than or equal to the maximum.
+ */
 export const max = (maximum: number, error?: ErrorMessage) =>
   check<number>(
     (val) => val <= maximum,
@@ -23,8 +40,19 @@ export const max = (maximum: number, error?: ErrorMessage) =>
       `Should be no more than ${maximum}`
     )
   );
+
+/**
+ * The meta key used to store the error message for this validation.
+ */
 max.ERROR = Symbol("number_max_error");
 
+/**
+ * Checks if a number is greater than or equal to the minimum.
+ *
+ * @param minimum - The minimum value.
+ * @param error - Optionally specify an error message to use in this case.
+ * @returns A validation pipeline that checks if a number is greater than or equal to the minimum.
+ */
 export const min = (minimum: number, error?: ErrorMessage) =>
   check<number>(
     (val) => val >= minimum,
@@ -35,6 +63,15 @@ export const min = (minimum: number, error?: ErrorMessage) =>
       `Should be at least ${minimum}`
     )
   );
+
+/**
+ * The meta key used to store the error message for this validation.
+ */
 min.ERROR = Symbol("number_min_error");
 
+/**
+ * Transforms a number to a string.
+ *
+ * @returns A validation pipeline that transforms a number to a string.
+ */
 export const toString = () => transform<number, string>((val) => String(val));
