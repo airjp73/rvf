65049d1a1788bdd425d13f5353332d830ae9b112
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-05-08 23:29:51 -0400

    fix: correct method typings
    There's some handwaving and `as any` going on in the methods themselves that would be nice to figure out, but the user-facing types are more important.

--- packages/experimental-validation/src/core.ts
+++ packages/experimental-validation/src/core.ts
@@ -3,36 +3,60 @@ import { Merge } from "./typeHelpers";
 
 export type AnyMeta = Record<string | number | symbol, any>;
 export type AnyMethods = Record<string, any>;
-export type AnySchema = Schema<any, any, {}, {}>;
+export type AnySchema = Schema<any, any, any, any>;
+
+type SchemaType<Input, Output, Meta, Methods> = Schema<
+  Input,
+  Output,
+  Meta,
+  Methods
+> &
+  Methods;
+
+type SchemaInput<T> = T extends BaseSchema<infer U, any, {}, {}> ? U : never;
+type SchemaOutput<T> = T extends BaseSchema<any, infer U, {}, {}> ? U : never;
+type SchemaMeta<T> = T extends BaseSchema<any, any, infer U, {}> ? U : never;
+type SchemaMethods<T> = T extends BaseSchema<any, any, {}, infer U> ? U : never;
+
+export class BaseSchema<
+  Input,
+  Output,
+  Meta extends AnyMeta,
+  Methods extends AnyMethods
+> {
+  protected perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>;
+  protected methods: Methods;
+  protected meta: Meta;
+  protected parent: AnySchema | undefined;
+
+  protected constructor(
+    perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>,
+    meta: Meta,
+    methods: Methods,
+    parent?: Schema<Input, Output, Meta, Methods>
+  ) {
+    this.meta = meta;
+    this.perform = perform;
+    this.methods = methods;
+    this.parent = parent;
+  }
+}
 
 export class Schema<
   Input,
   Output,
   Meta extends AnyMeta,
   Methods extends AnyMethods
-> {
+> extends BaseSchema<Input, Output, Meta, Methods> {
   static of<Input, Output, Meta extends AnyMeta, Methods extends AnyMethods>(
     perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>,
     meta: Meta,
     methods: Methods,
-    parent?: Schema<Input, Output, Meta, Methods>
+    parent?: AnySchema
   ): Schema<Input, Output, Meta, Methods> & Methods {
     // There really isn't a way to do this in a truly typesafe way as far as I can tell.
     // Object.keys isn't typesafe, so we have to hand wave a bit here.
     return new Schema(perform, meta, methods, parent) as any;
-  }
-
-  #perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>;
-  #methods: Methods;
-  #meta: Meta;
-  #parent: Schema<Input, Output, Meta, Methods> | undefined;
-
-  get meta() {
-    return this.#meta;
-  }
-
-  get parent() {
-    return this.#parent;
   }
 
   private constructor(
@@ -41,10 +65,7 @@ export class Schema<
     methods: Methods,
     parent?: Schema<Input, Output, Meta, Methods>
   ) {
-    this.#meta = meta;
-    this.#perform = perform;
-    this.#methods = methods;
-    this.#parent = parent;
+    super(perform, meta, methods, parent);
 
     for (const [key, value] of Object.entries(methods)) {
       Object.defineProperty(this, key, {
@@ -56,18 +77,18 @@ export class Schema<
 
   withMeta<NextMeta extends AnyMeta>(nextMeta: NextMeta) {
     return Schema.of<Input, Output, Merge<Meta, NextMeta>, Methods>(
-      this.#perform,
-      { ...this.#meta, ...nextMeta },
-      this.#methods,
+      this.perform,
+      { ...this.meta, ...nextMeta },
+      this.methods,
       this
     );
   }
 
   withMethods<NextMethods extends AnyMethods>(nextMethods: NextMethods) {
     return Schema.of<Input, Output, Meta, Merge<Methods, NextMethods>>(
-      this.#perform,
-      this.#meta,
-      { ...this.#methods, ...nextMethods },
+      this.perform,
+      this.meta,
+      { ...this.methods, ...nextMethods },
       this
     );
   }
@@ -75,28 +96,38 @@ export class Schema<
   check(
     doCheck: (val: Output, meta: Meta) => PossiblyPromise<boolean>,
     makeError: (val: Output, meta: Meta) => string
-  ) {
+  ): this {
     return Schema.of<Input, Output, Meta, Methods>(
       (input, meta) =>
         this.validateMaybeAsync(input, meta).then((output) => {
           if (doCheck(output, meta)) return output;
           throw new Error(makeError(output, meta));
         }),
-      this.#meta,
-      this.#methods,
+      this.meta,
+      this.methods,
       this
-    );
+    ) as this;
   }
 
   transform<NextOutput>(
     nextPerform: (input: Output, meta: Meta) => PossiblyPromise<NextOutput>
-  ) {
-    return Schema.of<Input, NextOutput, Meta, {}>(
+  ): SchemaType<
+    SchemaInput<this>,
+    NextOutput,
+    SchemaMeta<this>,
+    Record<string, never>
+  > {
+    return Schema.of<
+      SchemaInput<this>,
+      NextOutput,
+      SchemaMeta<this>,
+      Record<string, never>
+    >(
       (input, meta) =>
         this.validateMaybeAsync(input, meta).then((output) =>
           nextPerform(output, meta)
         ),
-      this.#meta,
+      this.meta as any,
       {},
       this
     );
@@ -104,20 +135,20 @@ export class Schema<
 
   as<NextOutput, NextMeta extends AnyMeta, NextMethods extends AnyMethods>(
     castTo: Schema<Output, NextOutput, NextMeta, NextMethods>
-  ) {
-    return Schema.of<Input, NextOutput, NextMeta, NextMethods>(
+  ): SchemaType<SchemaInput<this>, NextOutput, NextMeta, NextMethods> {
+    return Schema.of<SchemaInput<this>, NextOutput, NextMeta, NextMethods>(
       (input, meta) =>
         this.validateMaybeAsync(input, meta).then((output) =>
           castTo.validateMaybeAsync(output, meta)
         ),
-      { ...this.#meta, ...castTo.#meta },
-      castTo.#methods,
+      { ...this.meta, ...castTo.meta },
+      castTo.methods,
       this
     );
   }
 
   validateMaybeAsync(input: Input, meta: AnyMeta): MaybePromise<Output> {
-    return MaybePromise.of(() => this.#perform(input, meta));
+    return MaybePromise.of(() => this.perform(input, meta));
   }
 
   validate(input: Input, meta: AnyMeta): Promise<Output> {
@@ -128,3 +159,17 @@ export class Schema<
     return this.validateMaybeAsync(input, meta).assertSync();
   }
 }
+
+export const makeType = <Output, Methods extends AnyMethods>(
+  doCheck: (val: unknown, meta: AnyMeta) => val is Output,
+  makeError: (val: unknown, meta: AnyMeta) => string,
+  methods: Methods
+) =>
+  Schema.of(
+    (input: unknown, meta: AnyMeta) => {
+      if (doCheck(input, meta)) return input;
+      throw new Error(makeError(input, meta));
+    },
+    {},
+    methods
+  );
--- packages/experimental-validation/src/helpers/string.ts
+++ packages/experimental-validation/src/helpers/string.ts
@@ -1,40 +1,67 @@
-import { check, transform, typecheck } from "../core";
+import { AnySchema, makeType } from "../core";
 import { ErrorMessage, errorMessage } from "../errors";
 
+//////// Methods
+const stringMethods = {
+  /**
+   * Checks that the length of a string is less than or equal to the maximum.
+   * @param max The maximum length of the string.
+   * @param error Optional error message
+   * @returns A new schema with this validation added
+   */
+  maxLength<Self extends AnySchema>(this: Self, max: number, error?: string) {
+    return this.check(
+      (val) => val.length >= max,
+      errorMessage(
+        error,
+        string.ERRORS.MAX_LENGTH_ERROR,
+        (label) => `${label} should have no more than ${max} characters`,
+        `Should have no more than ${max} characters`
+      )
+    );
+  },
+
+  /**
+   * Checks that the length of a string is greater than or equal to the minimum.
+   * @param min The minimum length of the string.
+   * @param error Optional error message.
+   * @returns A new schema with this validation added.
+   */
+  minLength<Self extends AnySchema>(this: Self, min: number, error?: string) {
+    return this.check(
+      (val) => val.length <= min,
+      errorMessage(
+        error,
+        string.ERRORS.MIN_LENGTH_ERROR,
+        (label) => `${label} should have no more than ${min} characters`,
+        `Should have no more than ${min} characters`
+      )
+    );
+  },
+
+  /**
+   * Transforms the string into a number.
+   * @returns A new schema with this transformation added.
+   */
+  toNumber<Self extends AnySchema>(this: Self) {
+    return this.transform((val) => Number(val));
+  },
+};
+
 export const string = (typeError?: ErrorMessage) =>
-  typecheck(
+  makeType(
     (val): val is string => typeof val === "string",
     errorMessage(
       typeError,
-      string.ERROR,
+      string.ERRORS.TYPE_ERROR,
       (label) => `Expected ${label} to be a string`,
       "Expected a string"
-    )
+    ),
+    stringMethods
   );
-string.ERROR = Symbol("string_error");
 
-export const maxLength = (max: number, error?: string) =>
-  check<string>(
-    (val) => val.length >= max,
-    errorMessage(
-      error,
-      maxLength.ERROR,
-      (label) => `${label} should have no more than ${max} characters`,
-      `Should have no more than ${max} characters`
-    )
-  );
-maxLength.ERROR = Symbol("string_maxLength_error");
-
-export const minLength = (min: number, error?: string) =>
-  check<string>(
-    (val) => val.length <= min,
-    errorMessage(
-      error,
-      minLength.ERROR,
-      (label) => `${label} should have no more than ${min} characters`,
-      `Should have no more than ${min} characters`
-    )
-  );
-minLength.ERROR = Symbol("string_minLength_error");
-
-export const toNumber = () => transform<string, number>((val) => Number(val));
+string.ERRORS = {
+  TYPE_ERROR: Symbol("string_error"),
+  MAX_LENGTH_ERROR: Symbol("string_maxLength_error"),
+  MIN_LENGTH_ERROR: Symbol("string_minLength_error"),
+};
