671482e7c5e62689288eb974501c8d654294a7cb
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-05-08 22:28:34 -0400

    feat: switch cores

--- packages/experimental-validation/src/core.ts
+++ packages/experimental-validation/src/core.ts
@@ -2,94 +2,129 @@ import { MaybePromise, PossiblyPromise } from "./maybePromise";
 import { Merge } from "./typeHelpers";
 
 export type AnyMeta = Record<string | number | symbol, any>;
-export type AnyProps = any;
+export type AnyMethods = Record<string, any>;
+export type AnySchema = Schema<any, any, {}, {}>;
 
-export interface ValidationPipeline<Input, Output, Meta extends AnyMeta = {}> {
-  perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>;
-  validateMaybeAsync: (input: Input, meta: AnyMeta) => MaybePromise<Output>;
-  validateSync: (input: Input) => Output;
-  validateAsync: (input: Input) => Promise<Output>;
-  meta: Meta;
-  props?: AnyProps;
-
-  e<NewMeta extends AnyMeta>(
-    pipeline: ValidationPipeline<unknown, unknown, NewMeta>
-  ): ValidationPipeline<Input, Output, Merge<Meta, NewMeta>>;
-  e<NewOutput, NewMeta extends AnyMeta>(
-    pipeline: ValidationPipeline<Output, NewOutput, NewMeta>
-  ): ValidationPipeline<Input, NewOutput, Merge<Meta, NewMeta>>;
-}
-
-/**
- * Picks the last entry from the tuple or array.
- */
-export type Last<T extends any[]> = T extends [...any[], infer L] ? L : any;
-
-export function makeValidator<Input, Output>(
-  perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>
-): ValidationPipeline<Input, Output, {}>;
-export function makeValidator<Input, Output, Meta extends AnyMeta>(
-  perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>,
-  meta: Meta
-): ValidationPipeline<Input, Output, Meta>;
-export function makeValidator<
+export class Schema<
   Input,
   Output,
   Meta extends AnyMeta,
-  Props extends AnyProps
->(
-  perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>,
-  meta: Meta,
-  props: Props
-): ValidationPipeline<Input, Output, Meta>;
-export function makeValidator<Input, Output>(
-  perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>,
-  meta: AnyMeta = {},
-  props?: AnyProps
-): ValidationPipeline<Input, Output, AnyMeta> {
-  const validateMaybeAsync = (input: Input, passedMeta: AnyMeta) =>
-    MaybePromise.of(() => perform(input, passedMeta));
-  return {
-    perform,
-    validateMaybeAsync,
-    meta,
-    props,
-    validateSync: (input) => validateMaybeAsync(input, meta).assertSync(),
-    validateAsync: (input) => validateMaybeAsync(input, meta).await(),
-    e(pipe: any) {
-      return makeValidator(
-        (input: Input, passedMeta) =>
-          validateMaybeAsync(input, passedMeta).then((res) =>
-            pipe.validateMaybeAsync(res, passedMeta)
-          ),
-        { ...meta, ...pipe.meta },
-        { parent: this }
-      );
-    },
-  };
+  Methods extends AnyMethods
+> {
+  static of<Input, Output, Meta extends AnyMeta, Methods extends AnyMethods>(
+    perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>,
+    meta: Meta,
+    methods: Methods,
+    parent?: Schema<Input, Output, Meta, Methods>
+  ): Schema<Input, Output, Meta, Methods> & Methods {
+    // There really isn't a way to do this in a truly typesafe way as far as I can tell.
+    // Object.keys isn't typesafe, so we have to hand wave a bit here.
+    return new Schema(perform, meta, methods, parent) as any;
+  }
+
+  #perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>;
+  #methods: Methods;
+  #meta: Meta;
+  #parent: Schema<Input, Output, Meta, Methods> | undefined;
+
+  get meta() {
+    return this.#meta;
+  }
+
+  get parent() {
+    return this.#parent;
+  }
+
+  private constructor(
+    perform: (input: Input, meta: AnyMeta) => PossiblyPromise<Output>,
+    meta: Meta,
+    methods: Methods,
+    parent?: Schema<Input, Output, Meta, Methods>
+  ) {
+    this.#meta = meta;
+    this.#perform = perform;
+    this.#methods = methods;
+    this.#parent = parent;
+
+    for (const [key, value] of Object.entries(methods)) {
+      Object.defineProperty(this, key, {
+        value,
+        writable: false,
+      });
+    }
+  }
+
+  withMeta<NextMeta extends AnyMeta>(nextMeta: NextMeta) {
+    return Schema.of<Input, Output, Merge<Meta, NextMeta>, Methods>(
+      this.#perform,
+      { ...this.#meta, ...nextMeta },
+      this.#methods,
+      this
+    );
+  }
+
+  withMethods<NextMethods extends AnyMethods>(nextMethods: NextMethods) {
+    return Schema.of<Input, Output, Meta, Merge<Methods, NextMethods>>(
+      this.#perform,
+      this.#meta,
+      { ...this.#methods, ...nextMethods },
+      this
+    );
+  }
+
+  check(
+    doCheck: (val: Output, meta: Meta) => PossiblyPromise<boolean>,
+    makeError: (val: Output, meta: Meta) => string
+  ) {
+    return Schema.of<Input, Output, Meta, Methods>(
+      (input, meta) =>
+        this.validateMaybeAsync(input, meta).then((output) => {
+          if (doCheck(output, meta)) return output;
+          throw new Error(makeError(output, meta));
+        }),
+      this.#meta,
+      this.#methods,
+      this
+    );
+  }
+
+  transform<NextOutput>(
+    nextPerform: (input: Output, meta: Meta) => PossiblyPromise<NextOutput>
+  ) {
+    return Schema.of<Input, NextOutput, Meta, {}>(
+      (input, meta) =>
+        this.validateMaybeAsync(input, meta).then((output) =>
+          nextPerform(output, meta)
+        ),
+      this.#meta,
+      {},
+      this
+    );
+  }
+
+  as<NextOutput, NextMeta extends AnyMeta, NextMethods extends AnyMethods>(
+    castTo: Schema<Output, NextOutput, NextMeta, NextMethods>
+  ) {
+    return Schema.of<Input, NextOutput, NextMeta, NextMethods>(
+      (input, meta) =>
+        this.validateMaybeAsync(input, meta).then((output) =>
+          castTo.validateMaybeAsync(output, meta)
+        ),
+      { ...this.#meta, ...castTo.#meta },
+      castTo.#methods,
+      this
+    );
+  }
+
+  validateMaybeAsync(input: Input, meta: AnyMeta): MaybePromise<Output> {
+    return MaybePromise.of(() => this.#perform(input, meta));
+  }
+
+  validate(input: Input, meta: AnyMeta): Promise<Output> {
+    return this.validateMaybeAsync(input, meta).await();
+  }
+
+  validateSync(input: Input, meta: AnyMeta): Output {
+    return this.validateMaybeAsync(input, meta).assertSync();
+  }
 }
-
-export const typecheck = <T>(
-  doCheck: (val: unknown, meta: AnyMeta) => val is T,
-  makeError: (val: unknown, meta: AnyMeta) => string
-): ValidationPipeline<unknown, T> =>
-  makeValidator((val, meta) => {
-    if (doCheck(val, meta)) return val as T;
-    throw new Error(makeError(val, meta));
-  });
-
-export const check = <Input>(
-  doCheck: (val: Input) => PossiblyPromise<boolean>,
-  makeError: (val: Input, meta: AnyMeta) => string
-): ValidationPipeline<Input, Input> =>
-  makeValidator((val, meta) => {
-    if (doCheck(val)) return val;
-    throw new Error(makeError(val, meta));
-  });
-
-export const transform = <Input, Output>(
-  doTransform: (val: Input, meta: AnyMeta) => PossiblyPromise<Output>
-): ValidationPipeline<Input, Output> => makeValidator(doTransform);
-
-export const setMeta = <M extends AnyMeta>(meta: M) =>
-  makeValidator((val) => val, meta);
