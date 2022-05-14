7916477c27b2a825eab7b4031d6478db66f993ed
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-05-14 10:01:46 -0400

    clean: remove commented code

--- apps/test-app/app/entry.client.tsx
+++ apps/test-app/app/entry.client.tsx
@@ -1,14 +1,6 @@
 import { RemixBrowser } from "@remix-run/react";
 import React from "react";
 import { hydrate } from "react-dom";
-import { hydrateRoot } from "react-dom/client";
-
-// hydrateRoot(
-//   document,
-//   <React.StrictMode>
-//     <RemixBrowser />
-//   </React.StrictMode>
-// );
 
 // TODO: Switch to hydrateRoot when possible
 // https://github.com/remix-run/remix/issues/2570
