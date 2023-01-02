cca03d9d714fa27e495999e0c44090fcba6dff46
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-01-02 11:13:22 -0500

    fix: use lodash.get to revert default value functionality

--- packages/set-get/src/getPath.ts
+++ packages/set-get/src/getPath.ts
@@ -1,6 +1,5 @@
-import * as R from "remeda";
-import { stringToPathArray } from "./stringToPathArray";
+import get from "lodash.get";
 
-// pathOr types don't support deeper than 3 levels, but the code works
-export const getPath = (object: any, path: string) =>
-  R.pathOr(object, stringToPathArray(path) as any, undefined);
+export const getPath = (object: any, path: string) => {
+  return get(object, path);
+};
