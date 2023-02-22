edac3628a0ccd03d41c618faec6b7037a833160c
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-02-22 15:16:21 -0500

    fix: add "exports" to zfd package.json

--- packages/zod-form-data/tsup.config.ts
+++ packages/zod-form-data/tsup.config.ts
@@ -1,3 +1,23 @@
-import { config } from "tsup-config";
+import { defineConfig } from "tsup";
 
-export default config;
+export default defineConfig({
+  entry: ["src/index.ts"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  format: ["esm", "cjs"],
+  define: {
+    "import.meta.vitest": undefined,
+  } as any,
+  outExtension({ format }) {
+    switch (format) {
+      case "esm":
+        return { js: ".mjs" };
+      case "cjs":
+        return { js: ".js" };
+      default:
+        throw new Error("Unknown format");
+    }
+  },
+});
