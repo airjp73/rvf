333becb0012542ac3da13e010ab7c680dcdc1e73
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-06-14 10:06:45 -0400

    test: add unit test to ensure matching peer dependency versions

---
+++ packages/with-yup/src/with-yup.test.ts
@@ -0,0 +1,25 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+import * as semver from "semver";
+import { describe, it, expect } from "vitest";
+
+const packageDir = path.join(__dirname, "..");
+const packageJsonPath = path.join(packageDir, "package.json");
+const rvfPackageJsonPath = path.join(
+  packageDir,
+  "../remix-validated-form/package.json"
+);
+
+describe("peer dependecy version", () => {
+  it("should have a peer dependency version that matches the lastet version of RVF", async () => {
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf-8"));
+    const peerDependencyVersion =
+      packageJson.peerDependencies["remix-validated-form"];
+    const rvfPackageJson = JSON.parse(
+      await fs.readFile(rvfPackageJsonPath, "utf-8")
+    );
+    const rvfVersion = rvfPackageJson.version;
+
+    expect(semver.satisfies(rvfVersion, peerDependencyVersion)).toBe(true);
+  });
+});
--- packages/with-zod/src/with-zod.test.ts
+++ packages/with-zod/src/with-zod.test.ts
@@ -1,4 +1,7 @@
+import * as fs from "fs/promises";
+import * as path from "path";
 import { anyString } from "@remix-validated-form/test-utils";
+import * as semver from "semver";
 import { describe, it, expect } from "vitest";
 import { z } from "zod";
 import { withZod } from "./";
@@ -85,5 +88,26 @@ describe("withZod", () => {
       },
       submittedData: obj,
     });
+  });
+});
+
+const packageDir = path.join(__dirname, "..");
+const packageJsonPath = path.join(packageDir, "package.json");
+const rvfPackageJsonPath = path.join(
+  packageDir,
+  "../remix-validated-form/package.json"
+);
+
+describe("peer dependecy version", () => {
+  it("should have a peer dependency version that matches the lastet version of RVF", async () => {
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf-8"));
+    const peerDependencyVersion =
+      packageJson.peerDependencies["remix-validated-form"];
+    const rvfPackageJson = JSON.parse(
+      await fs.readFile(rvfPackageJsonPath, "utf-8")
+    );
+    const rvfVersion = rvfPackageJson.version;
+
+    expect(semver.satisfies(rvfVersion, peerDependencyVersion)).toBe(true);
   });
 });
