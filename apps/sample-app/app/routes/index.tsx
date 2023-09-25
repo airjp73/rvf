e0604b493f53c3815d3404d720fdd66f6de185b8
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-09-25 08:40:52 -0400

    clean: remove sample-app

--- apps/sample-app/app/routes/index.tsx
+++
@@ -1,5 +0,0 @@
-import { DataFunctionArgs, redirect } from "@remix-run/node";
-
-export let loader = async (args: DataFunctionArgs) => {
-  return redirect("/subjects");
-};
--- apps/sample-app/app/types.ts
+++
@@ -1,6 +0,0 @@
-import { Subject, SubjectDays, Teacher } from "@prisma/client";
-
-export type SubjectComplete = Subject & {
-  teacher: Teacher;
-  subjectDays: SubjectDays[];
-};
