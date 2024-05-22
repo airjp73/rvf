6cb6f8cebb3fb37df471f7fa095b9b82ad330b0d
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-05-21 23:38:55 -0400

    fix: exclude built tets

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, "./**/*.config.ts", "./**/dist"],
-    includeSource: ["./**/*.{ts,tsx}"],
+    exclude: [
+      ...configDefaults.exclude,
+      "./**/*.config.ts",
+      "./**/dist",
+      "./**/build",
+    ],
     environment: "jsdom",
     globals: true,
     setupFiles: ["./vitest.setup.ts"],
