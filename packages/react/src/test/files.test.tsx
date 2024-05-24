46973c148c6c6dc9b2bdfe009dbfb32c72918326
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-05-24 15:10:28 -0400

    test: add a check for file inputs

---
+++ packages/react/src/test/files.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { successValidator } from "./util/successValidator";
+import { useRvf } from "../useRvf";
+import userEvent from "@testing-library/user-event";
+
+it("should be able to submit file inputs", async () => {
+  const submit = vi.fn();
+  const TestComp = () => {
+    const form = useRvf({
+      defaultValues: {
+        file: undefined as File | undefined,
+      },
+      validator: successValidator,
+      handleSubmit: submit,
+    });
+
+    return (
+      <form {...form.getFormProps()}>
+        <input data-testid="file" name="file" type="file" />
+        <button data-testid="submit" type="submit" />
+      </form>
+    );
+  };
+
+  render(<TestComp />);
+
+  const file = new File(["test"], "test.txt", { type: "text/plain" });
+  await userEvent.upload(screen.getByTestId("file"), file);
+
+  await userEvent.click(screen.getByTestId("submit"));
+
+  expect(submit).toHaveBeenCalledTimes(1);
+  expect(submit).toHaveBeenCalledWith({ file }, expect.any(FormData));
+});
