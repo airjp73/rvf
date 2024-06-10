8d20952d3081878ca6af3100dbbaf9b0d2f705ca
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-09 23:15:52 -0400

    clean: remove old constants

--- packages/remix/src/constants.ts
+++ packages/remix/src/constants.ts
@@ -1,5 +1,1 @@
 export const FORM_ID_FIELD_NAME = "__internal_rvf_form_id__";
-export const formDefaultValuesKey = (formId: string) =>
-  `__internal_rvf_defaults__${formId}`;
-
-export type FormDefaultsKey = `__internal_rvf_defaults__${string}`;
