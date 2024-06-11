ec8c427faa69dd951542482a56c2f216ef44d4ac
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-11 00:07:46 -0400

    chore: mdx plugin

--- apps/docs-v2/vite.config.ts
+++ apps/docs-v2/vite.config.ts
@@ -1,9 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mdx from "@mdx-js/rollup";
 
 export default defineConfig({
   plugins: [
+    { enforce: "pre", ...mdx() },
     remix({
       future: {
         v3_fetcherPersist: true,
