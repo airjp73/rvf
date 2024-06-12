64663788ecd1f9d3b91afc697e1118057521f148
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-11 23:03:46 -0400

    feat: fancy blurry header

--- apps/docs-v2/app/ui/layout/Header.tsx
+++ apps/docs-v2/app/ui/layout/Header.tsx
@@ -21,7 +21,10 @@ export const Header = forwardRef<
       ref={ref}
       className={clsx(
         className,
-        "fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80"
+        "fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80",
+        "backdrop-blur-sm lg:left-72 xl:left-80 dark:backdrop-blur",
+        "bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]",
+        "border-b border-border"
       )}
       style={
         {
