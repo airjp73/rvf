f7db3bbf025a5e8f954b08573547c7c4ed87d968
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-13 15:19:01 -0400

    test: add scoping test

---
+++ packages/core/src/form.test.ts
@@ -0,0 +1,38 @@
+import { createValidator } from "./createValidator";
+import { createFormScope } from "./form";
+
+it("should be able to nest scopes arbitrarily deep", () => {
+  const form = createFormScope({
+    defaultValues: {
+      foo: {
+        bar: {
+          baz: "",
+        },
+      },
+    },
+    serverValidationErrors: {},
+    validator: createValidator({
+      validate: (data) => Promise.resolve({ data, error: undefined }),
+    }),
+    submitSource: "state",
+    onSubmit: vi.fn(),
+    onSubmitSuccess: vi.fn(),
+    onSubmitFailure: vi.fn(),
+    flags: {
+      disableFocusOnError: false,
+      reloadDocument: false,
+    },
+    formProps: { id: "test-form" },
+  });
+
+  expect(form.__field_prefix__).toBe("");
+
+  const foo = form.scope("foo");
+  expect(foo.__field_prefix__).toBe("foo");
+
+  const bar = foo.scope("bar");
+  expect(bar.__field_prefix__).toBe("foo.bar");
+
+  const baz = bar.scope("baz");
+  expect(baz.__field_prefix__).toBe("foo.bar.baz");
+});
