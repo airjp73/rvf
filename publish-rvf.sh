16c17c310bdad3cc31d2a60ee3d2b5b15009397a
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-18 16:22:18 -0400

    chore: create publish script

---
+++ publish-rvf.sh
@@ -0,0 +1,23 @@
+cd packages/core
+npm version $1
+npm publish
+
+cd ../react
+npm version $1
+npm publish
+
+cd ../remix
+npm version $1
+npm publish
+
+cd ../yup
+npm version $1
+npm publish
+
+cd ../zod
+npm version $1
+npm publish
+
+cd ../set-get
+npm version $1
+npm publish
