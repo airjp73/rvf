83e24ff2c8496cf6d8a4b4aa11c19ad5464984b6
scythewyvern <vosckaniangor@gmail.com>
2024-08-27 12:07:43 +0300

    feat: add `valibot` adapter

---
+++ packages/valibot/src/index.ts
@@ -0,0 +1,49 @@
+import { type FieldErrors, type Validator, createValidator } from "@rvf/core";
+import {
+	type Config,
+	type GenericSchema,
+	type InferIssue,
+	type InferOutput,
+	flatten,
+	safeParseAsync,
+} from "valibot";
+
+function parseIssues<Schema extends GenericSchema>(
+	issues: [InferIssue<Schema>, ...InferIssue<Schema>[]],
+): FieldErrors {
+	const transformedObj: FieldErrors = {};
+	const flattenErrors = flatten<Schema>(issues);
+
+	if (!flattenErrors.nested) return transformedObj;
+
+	const regex = /\.(\d+)/g;
+
+	for (const [key, value] of Object.entries(flattenErrors.nested)) {
+		const transformedKey = key.replace(regex, "[$1]");
+		const transformedValue = Array.isArray(value) ? value[0] : "";
+
+		if (!transformedValue) continue;
+
+		transformedObj[transformedKey] = transformedValue;
+	}
+
+	return transformedObj;
+}
+
+export function withValibot<Schema extends GenericSchema>(
+	schema: Schema,
+	config?: Config<InferIssue<Schema>>,
+): Validator<InferOutput<Schema>> {
+	return createValidator({
+		validate: async (input) => {
+			const result = await safeParseAsync(schema, input, {
+				...config,
+				abortPipeEarly: true,
+			});
+
+			if (result.success) return { data: result.output, error: undefined };
+
+			return { data: undefined, error: parseIssues(result.issues) };
+		},
+	});
+}
