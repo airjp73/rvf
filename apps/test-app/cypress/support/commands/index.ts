87d878a070d47ca1a53427808ae82acff3c34f6c
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-12-18 09:38:18 -0500

    fix: hydration error

--- apps/test-app/cypress/support/commands/index.ts
+++ apps/test-app/cypress/support/commands/index.ts
@@ -22,3 +22,16 @@ Cypress.Commands.add("visitWithoutJs", (url) => {
 Cypress.Commands.add("waitForJs", () => {
   return cy.findByTestId("hydrated");
 });
+
+Cypress.on("uncaught:exception", (err) => {
+  // Cypress and React Hydrating the document don't get along
+  // for some unknown reason. Hopefully we figure out why eventually
+  // so we can remove this.
+  if (
+    /hydrat/i.test(err.message) ||
+    /Minified React error #418/.test(err.message) ||
+    /Minified React error #423/.test(err.message)
+  ) {
+    return false;
+  }
+});
