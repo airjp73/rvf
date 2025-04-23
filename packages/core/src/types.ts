953a017c81d9932a2e792864e1a9ec606cf38a77
Aaron Pettengill <pettengill.aaron@gmail.com>
2025-04-23 16:22:07 -0400

    chore: copy over to core package

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,3 +1,4 @@
+import * as h from "hotscript";
 import { GenericObject } from "./native-form-data/flatten";
 
 export type FieldValues = Record<string | number, any>;
@@ -109,55 +110,95 @@ type IsUnknown<T> = unknown extends T // `T` can be `unknown` or `any`
     ? true
     : false
   : false;
+// https://github.com/sindresorhus/type-fest/blob/86a3a6929f87948f708126083bfb760582e48989/source/is-never.d.ts#L42
+type IsNever<T> = [T] extends [never] ? true : false;
+interface IsNeverFn extends h.Fn {
+  return: IsNever<this["arg0"]>;
+}
 
-type HandleObjects<T, U> = {
+type Tuple<T = any> = [...T[]];
+
+interface HasMatchIn<T extends Tuple> extends h.Fn {
+  return: h.Pipe<
+    T,
+    [
+      h.Tuples.Find<h.Booleans.Equals<this["arg0"]>, T>,
+      IsNeverFn,
+      h.Booleans.Not,
+    ]
+  >;
+}
+
+type ToUnion<T extends Tuple> = h.Call<h.Tuples.ToUnion<T>>;
+
+type ReconcileObjects<T extends object, U extends object> = {
   [K in keyof T | keyof U]: K extends keyof T
     ? K extends keyof U
       ? NonContradictingSupertype<T[K], U[K]>
       : T[K]
-    : K extends keyof U
-      ? U[K]
-      : never;
+    : never;
 };
 
-type AnyReadStatus<T> = T | Readonly<T>;
-type HandleTuples<T, U> = T extends [infer THead, ...infer TTail]
-  ? U extends AnyReadStatus<[infer UHead, ...infer UTail]>
-    ? [NonContradictingSupertype<THead, UHead>, ...HandleTuples<TTail, UTail>]
-    : [THead, ...TTail]
-  : T;
+interface NestedSupertypeFn extends h.Fn {
+  return: this["arg0"] extends [infer one, infer two]
+    ? NonContradictingSupertype<one, two>
+    : never;
+}
 
-type Primitive = string | number | boolean | symbol | bigint | null | undefined;
-
-type NonDistributiveExtends<One, Two> = One extends Two ? true : false;
-type HandlePrimitives<T, U> = NonDistributiveExtends<T, U> extends true ? U : T;
-type Tuple = AnyReadStatus<[any, ...any[]]>;
-type AnyArray<T = any> = AnyReadStatus<Array<T>>;
+type ReconcileTuple<T extends Tuple, U extends Tuple> =
+  h.Call<
+    h.Booleans.Equals<h.Call<h.Tuples.Length<T>>, h.Call<h.Tuples.Length<U>>>
+  > extends true
+    ? h.Pipe<h.Call<h.Tuples.Zip<T, U>>, [h.Tuples.Map<NestedSupertypeFn>]>
+    : T;
 
 // prettier-ignore
-type HandleDifferences<T, U> =
-  [T, U] extends [Primitive, Primitive]
-    ? HandlePrimitives<T, U>
-  : [T, U] extends [Primitive, any] | [any, Primitive]
-    ? T
-  : [T, U] extends [Tuple, Tuple]
-    ? HandleTuples<T, U>
-  : [T, U] extends [Tuple, any] | [any, Tuple]
-    ? T
-  : [T, U] extends [AnyArray<infer TItem>, AnyArray<infer UItem>]
-    ? Array<NonContradictingSupertype<TItem, UItem>>
-  : [T, U] extends [AnyArray, any] | [any, AnyArray]
-    ? T
-  : Prettify<HandleObjects<T, U>>;
+type Reconcile<T, U> =
+  T extends Primitive ? U extends Primitive
+    ? T extends U ? U : T
+    : T
+  : T extends AnyReadStatus<[infer THead, ...infer TTail]>
+    ? U extends AnyReadStatus<[infer UHead, ...infer UTail]>
+      ? ReconcileTuple<[THead, ...TTail], [UHead, ...UTail]>
+    : T
+  : T extends AnyReadStatus<Array<infer TItem>>
+    ? U extends AnyReadStatus<Array<infer UItem>>
+      ? Array<NonContradictingSupertype<TItem, UItem>>
+    : U extends AnyReadStatus<[infer UHead, ...infer UTail]>
+      ? Array<NonContradictingSupertype<TItem, ToUnion<[UHead, ...UTail]>>>
+    : T
+  : T extends object
+    ? U extends object ? ReconcileObjects<T, U>
+    : T
+  : T
 
-/**
- * Finds the least common supertype with some restrictions.
- */
+type HandleDifferences<T extends Tuple, U extends Tuple> = T extends []
+  ? ToUnion<U>
+  : [T, U] extends [[infer TOnly], [infer UOnly]]
+    ? Reconcile<TOnly, UOnly>
+    : ToUnion<T>;
+
+type Work<
+  T,
+  U,
+  TTuple extends Tuple = h.Call<h.Unions.ToTuple, T>,
+  UTuple extends Tuple = h.Call<h.Unions.ToTuple, U>,
+> = [
+  h.Pipe<TTuple, [h.Tuples.Partition<HasMatchIn<UTuple>>]>,
+  h.Pipe<UTuple, [h.Tuples.Partition<HasMatchIn<TTuple>>]>,
+] extends [
+  [infer TExact, infer TDiff extends Tuple],
+  [any, infer UDiff extends Tuple],
+]
+  ? h.Call<h.Tuples.ToUnion, TExact> | Prettify<HandleDifferences<TDiff, UDiff>>
+  : never;
+
+// /**
+//  * Finds the least common supertype with some restrictions.
+//  */
 export type NonContradictingSupertype<T, U> =
-  IsUnknown<T> extends true
-    ? U
-    : T extends U
-      ? U extends T
-        ? U
-        : HandleDifferences<T, U>
-      : HandleDifferences<T, U>;
+  IsUnknown<T> extends true ? U : Prettify<Work<T, U>>;
+
+type AnyReadStatus<T> = T | Readonly<T>;
+
+type Primitive = string | number | boolean | symbol | bigint | null | undefined;
--- packages/react/src/useForm.tsx
+++ packages/react/src/useForm.tsx
@@ -11,118 +11,11 @@ import {
   DomSubmitHandler,
   BeforeSubmitApi,
   withStandardSchema,
-  // NonContradictingSupertype,
+  NonContradictingSupertype,
 } from "@rvf/core";
 import { FormApi, useFormInternal } from "./base";
 import { FieldErrors } from "@rvf/core";
 import { StandardSchemaV1 } from "@standard-schema/spec";
-
-type Prettify<T> = {
-  [K in keyof T]: T[K];
-  // necessary for this type
-  // eslint-disable-next-line @typescript-eslint/ban-types
-} & {};
-
-// https://github.com/sindresorhus/type-fest/blob/44c1766504a2a5024f063ac83bc67d28ec52cba9/source/is-null.d.ts
-type IsNull<T> = [T] extends [null] ? true : false;
-// https://github.com/sindresorhus/type-fest/blob/44c1766504a2a5024f063ac83bc67d28ec52cba9/source/is-unknown.d.ts
-type IsUnknown<T> = unknown extends T // `T` can be `unknown` or `any`
-  ? IsNull<T> extends false // `any` can be `null`, but `unknown` can't be
-    ? true
-    : false
-  : false;
-// https://github.com/sindresorhus/type-fest/blob/86a3a6929f87948f708126083bfb760582e48989/source/is-never.d.ts#L42
-type IsNever<T> = [T] extends [never] ? true : false;
-interface IsNeverFn extends h.Fn {
-  return: IsNever<this["arg0"]>;
-}
-
-type Tuple<T = any> = [...T[]];
-
-interface HasMatchIn<T extends Tuple> extends h.Fn {
-  return: h.Pipe<
-    T,
-    [
-      h.Tuples.Find<h.Booleans.Equals<this["arg0"]>, T>,
-      IsNeverFn,
-      h.Booleans.Not,
-    ]
-  >;
-}
-
-type ToUnion<T extends Tuple> = h.Call<h.Tuples.ToUnion<T>>;
-
-type ReconcileObjects<T extends object, U extends object> = {
-  [K in keyof T | keyof U]: K extends keyof T
-    ? K extends keyof U
-      ? NonContradictingSupertype<T[K], U[K]>
-      : T[K]
-    : never;
-};
-
-interface NestedSupertypeFn extends h.Fn {
-  return: this["arg0"] extends [infer one, infer two]
-    ? NonContradictingSupertype<one, two>
-    : never;
-}
-
-type ReconcileTuple<T extends Tuple, U extends Tuple> =
-  h.Call<
-    h.Booleans.Equals<h.Call<h.Tuples.Length<T>>, h.Call<h.Tuples.Length<U>>>
-  > extends true
-    ? h.Pipe<h.Call<h.Tuples.Zip<T, U>>, [h.Tuples.Map<NestedSupertypeFn>]>
-    : T;
-
-// prettier-ignore
-type Reconcile<T, U> =
-  T extends Primitive ? U extends Primitive
-    ? T extends U ? U : T
-    : T
-  : T extends AnyReadStatus<[infer THead, ...infer TTail]>
-    ? U extends AnyReadStatus<[infer UHead, ...infer UTail]>
-      ? ReconcileTuple<[THead, ...TTail], [UHead, ...UTail]>
-    : T
-  : T extends AnyReadStatus<Array<infer TItem>>
-    ? U extends AnyReadStatus<Array<infer UItem>>
-      ? Array<NonContradictingSupertype<TItem, UItem>>
-    : U extends AnyReadStatus<[infer UHead, ...infer UTail]>
-      ? Array<NonContradictingSupertype<TItem, ToUnion<[UHead, ...UTail]>>>
-    : T
-  : T extends object
-    ? U extends object ? ReconcileObjects<T, U>
-    : T
-  : T
-
-type HandleDifferences<T extends Tuple, U extends Tuple> = T extends []
-  ? ToUnion<U>
-  : [T, U] extends [[infer TOnly], [infer UOnly]]
-    ? Reconcile<TOnly, UOnly>
-    : ToUnion<T>;
-
-type Work<
-  T,
-  U,
-  TTuple extends Tuple = h.Call<h.Unions.ToTuple, T>,
-  UTuple extends Tuple = h.Call<h.Unions.ToTuple, U>,
-> = [
-  h.Pipe<TTuple, [h.Tuples.Partition<HasMatchIn<UTuple>>]>,
-  h.Pipe<UTuple, [h.Tuples.Partition<HasMatchIn<TTuple>>]>,
-] extends [
-  [infer TExact, infer TDiff extends Tuple],
-  [any, infer UDiff extends Tuple],
-]
-  ? h.Call<h.Tuples.ToUnion, TExact> | Prettify<HandleDifferences<TDiff, UDiff>>
-  : never;
-
-// /**
-//  * Finds the least common supertype with some restrictions.
-//  */
-export type NonContradictingSupertype<T, U> =
-  IsUnknown<T> extends true ? U : Prettify<Work<T, U>>;
-
-type AnyReadStatus<T> = T | Readonly<T>;
-
-type Primitive = string | number | boolean | symbol | bigint | null | undefined;
 
 ///////////
 
