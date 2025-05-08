1a864f2ebfeb27b83bf8cfe980ce45f18f20a51d
Aaron Pettengill <pettengill.aaron@gmail.com>
2025-05-08 10:44:21 -0400

    fix: new bug with vitest & node 22.15

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom/vitest";
+import { URLSearchParams } from "node:url";
+
+// https://github.com/vitest-dev/vitest/issues/7906
+// @ts-expect-error
+globalThis.URLSearchParams = URLSearchParams;
